8f1c2cb3997af5632bf447ebc3c7c85b8b4deca6
Thorsten Töpper <atsutane@freethoughts.de>
2011-01-10 20:49:33 +0100

    Aufgabe 2: monitor implementiert.

--- aufgabe2/monitor.c
+++ aufgabe2/monitor.c
@@ -1,18 +1,49 @@
 /*
  * vim:ts=4:sw=4:expandtab
- * 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
+#include "queue.h"
+
+FILE *pipe_stat;
+char *message;
+
 void monitor() {
+    int buffer;
+
     printf("monitor started\n");
-    for (;;) {
+
+    /* Die Pipe zum lesen öffnen */
+    if ((pipe_stat = fdopen(queue[D_STAT_TO_MON][READ], "r")) == NULL) {
+        perror("monitor.c fdopen()");
+        exit(EXIT_FAILURE);
     }
+
+    /* Speicher für Nachricht reservieren */
+    if ((message = calloc(1, sizeof(char)*SIZE_HEX+1)) == NULL) {
+        perror("monitor.c calloc()");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Nachricht aus der Pipe lesen, zurück ins Dezimalsystem
+     * konvertieren und auf der stdout ausgeben.
+     */
+    while (fgets(message, SIZE_HEX+1, pipe_stat)) {
+        sscanf(message, "%x\n", &buffer);
+
+        printf("%d\n", buffer);
+    }
+
 }
 
 void monitor_cleanup() {
     printf("monitor cleanup\n");
+    fclose(pipe_stat);
+    free(message);
+    close(queue[D_STAT_TO_MON][READ]);
     _exit(EXIT_SUCCESS);
 }
